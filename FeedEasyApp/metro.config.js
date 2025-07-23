9bc72e1ae52d2ff27f22b2fc722dc0d27d908e18
kisirinya sirajje <kisirinyasiraje@gmail.com>
2025-07-23 09:18:11 +0300

    deletes

--- FeedEasyApp/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure TypeScript files are properly handled
-config.resolver.sourceExts.push('tsx', 'ts');
-
-module.exports = config;
